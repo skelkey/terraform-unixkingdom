2dd1d4240fad2cec4a34974cac2a0095c3fe4c3a
Edouard Camoin <edouard.camoin@gmail.com>
2020-01-07 21:38:07 +0000

    Creating LBU for vault instance

--- vault.tf
+++ vault.tf
@@ -85,3 +85,81 @@ resource "osc_security_group" "vault" {
     Name    = "euw2-prd-unixkingdom-vault"
   }
 }
+
+resource "osc_elb" "vault-lbu" {
+  name            = "euw2-unixkingdom-vault-lbu"
+
+  security_groups = [ "${osc_security_group.vault-lbu.id}" ]
+  subnets         = [ "${osc_subnet.euw2-unixkingdom-public.id}" ]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+  listener {
+    instance_port     = 443
+    instance_protocol = "TCP"
+    lb_port           = 443
+    lb_protocol       = "TCP"
+  } 
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "TCP:443"
+    interval            = 30
+  }
+
+  tags {
+    Name = "euw2a-prd-unixkingdom-repository-lbu"
+  }
+}
+
+resource "osc_security_group" "vault-lbu" {
+  name        = "euw2-prd-unixkingdom-vault-lbu"
+  description = "euw2-prd-unixkingdom-vault-lbu"
+
+  ingress = {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  ingress = {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0" ]
+  }
+
+  ingress = {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  vpc_id = "${osc_vpc.euw2-unixkingdom-network.id}"
+}
+
+resource "osc_elb_attachment" "vault-attachment" {
+   elb = "${osc_elb.vault-lbu.id}"
+   instance = "${osc_instance.vault-1.id}"
+} 
+
