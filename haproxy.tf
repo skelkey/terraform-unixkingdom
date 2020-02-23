5dd854af33dfb51606d941dc41a200bdeed3aeef
Edouard Camoin <edouard.camoin@gmail.com>
2020-02-23 21:22:22 +0000

    Haproxy for administration network

---
+++ haproxy.tf
@@ -0,0 +1,91 @@
+data "template_cloudinit_config" "haproxy_config" {
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.minion.rendered}"
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    content      = <<EOF
+        preserve_hostname: false
+        hostname: euw2a-prd-unixkingdom-haproxy-1
+        fqdn: euw2a-prd-unixkingdom-haproxy-1
+        manage_etc_hosts: true
+    EOF
+  }
+}
+
+resource "osc_instance" "haproxy-1" {
+  ami               = "${var.ami}"
+  availability_zone = "${var.region}a"
+  instance_type     = "c4.large"
+  key_name          = "${var.sshkey}"
+
+  vpc_security_group_ids = [
+    "${osc_security_group.haproxy.id}",
+  ]
+
+  subnet_id = "${osc_subnet.euw2-unixkingdom-administration.id}"
+
+  tags {
+    Name = "euw2a-prd-unixkingdom-haproxy-1"
+  }
+
+  user_data = "${data.template_cloudinit_config.haproxy_config.rendered}"
+}
+
+output "haproxy-1" {
+  value = "${osc_instance.haproxy-1.private_ip}"
+}
+
+resource "osc_security_group" "haproxy" {
+  name = "euw2-prd-unixkingdom-haproxy"
+  description = "euw2-prd-unixkingdom-haproxy"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "${var.lan_subnet}",
+    ]
+
+    security_groups = [
+      "${osc_security_group.euw2-prd-unixkingdom-strongswan.id}",
+    ]
+  }
+
+  ingress {
+    from_port = -1
+    to_port   = -1
+    protocol  = "icmp"
+
+    cidr_blocks = [
+      "0.0.0.0/0",
+    ]
+  }
+
+  ingress {
+    from_port = 4505
+    to_port   = 4506
+    protocol  = "tcp"
+
+    cidr_blocks = [
+      "${osc_instance.euw2a-prd-unixkingdom-saltstack-1.private_ip}",
+    ]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  vpc_id = "${osc_vpc.euw2-unixkingdom-network.id}"
+
+  tags {
+    Name    = "euw2-prd-unixkingdom-haproxy"
+  }
+}
