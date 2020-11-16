b85360ec5c7b2ca63cb7b670e8f180fcd26021a8
Edouard Camoin <edouard.camoin@gmail.com>
2020-11-16 13:02:42 +0000

    Adding passbolt flow to ldap

--- ldap.tf
+++ ldap.tf
@@ -44,7 +44,8 @@ resource "osc_security_group" "euw2-prd-unixkingdom-ldap" {
 
     security_groups = [
       "${osc_security_group.euw2-prd-unixkingdom-webadm.id}",
-      "${osc_security_group.radius.id}"
+      "${osc_security_group.radius.id}",
+      "${osc_security_group.passbolt.id}"
     ]
 
     cidr_blocks = [
